20fa7820dea31796ba2859e2dc5733c961737f8f
Veniamin-Gi <golubveniamin41@gmail.com>
2021-11-22 17:37:04 +0200

    Add icons

--- js/Scroll.js
+++ js/Scroll.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(".header__item").on("click","a", function (event) {
+	$(".nav-link").on("click","a", function (event) {
 		//отменяем стандартную обработку нажатия по ссылке
 		event.preventDefault();
 
@@ -36,33 +36,3 @@ $(document).ready(function(){
     });
   });
 
-
-  function addTask () {
-	var input = document.getElementById("input");
-	// get current text from input field
-	var newTask = input.value;
-	// only add new item to list if some text was entered
-	if (newTask != "") {
-	// create new HTML list item
-	var item = document.createElement("li");
-	// add HTML for buttons and new task text
-	// Note, need to use '' because of "" in HTML
-	item.innerHTML = '<input type="button" class="done" onclick="markDone(this.parentNode)" value="&#x2713;" /> ' + '<input type="button" class="remove" onclick="remove(this.parentNode)" value="&#x2715;" /> ' + newTask;
-	// add new item as part of existing list
-	document.getElementById("tasks").appendChild(item);
-	/* Step 4 below here */
-	input.value = "";
-	input.ariaPlaceholder = "enter next task...";
-	}
-	}
-	// change styling used for given item
-	function markDone (item) {
-	item.className = 'finished';
-	}
-	/* Step 7 below here */
-	function remove (item) {
-	// remove item completely from document
-	if (item.className == 'finished'){
-	item.remove();
-	}
-}
