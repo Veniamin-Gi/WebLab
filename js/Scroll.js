633ce5e4baa7060ad937d46480fc2490f71c6cd6
Veniamin-Gi <golubveniamin41@gmail.com>
2021-11-22 16:34:54 +0200

    New Page+ New css

--- js/Scroll.js
+++ js/Scroll.js
@@ -10,13 +10,27 @@ $(document).ready(function(){
 			top = $(id).offset().top;
 		
 		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
+		
 	});
+	$('body,html').animate({scrollTop: top}, 1500);
+		$("#start").on("click","a", function (event) {
+			//отменяем стандартную обработку нажатия по ссылке
+			event.preventDefault();
+	
+			//забираем идентификатор бока с атрибута href
+			var id  = $(this).attr('href'),
+	
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+				top = $(id).offset().top;
+			
+			//анимируем переход на расстояние - top за 1500 мс
+			$('body,html').animate({scrollTop: top}, 200);
+		});
 });
 
 $(document).ready(function(){
-    $("button").click(function(){
-      $("div").animate({
+    $("#start").click(function(){
+      $(".work").animate({
         height: 'toggle'
       });
     });
